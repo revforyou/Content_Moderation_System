be6156721c38f224df1d3361da5f770d1a6987b9
krish9164 <112063273+krish9164@users.noreply.github.com>
2025-05-08 00:30:04 -0400

    Create split_by_timestamp.py

---
+++ data_pipeline/scripts/split_by_timestamp.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("train.csv", usecols=["comment_text", "target", "created_date"])
+df["created_date"] = pd.to_datetime(df["created_date"])
+df = df.sort_values("created_date")
+
+split_index = int(len(df) * 0.9)
+train_val = df.iloc[:split_index]
+production = df.iloc[split_index:]
+
+train, val = train_test_split(train_val, test_size=0.1111, random_state=42)
+
+train.to_csv("train.csv", index=False)
+val.to_csv("val.csv", index=False)
+production.to_csv("production.csv", index=False)
