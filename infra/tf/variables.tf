18b4aebedbbcf5ff7c3911a9c948aff8cad0c1ce
krish9164 <112063273+krish9164@users.noreply.github.com>
2025-05-11 09:36:46 -0400

    Update variables.tf

--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -1,8 +1,20 @@
-variable "auth_url"       {}
-variable "os_username"    {}
-variable "os_password"    {}
-variable "project_name"   {}
-variable "region"         {}
-variable "image_name"     {}
-variable "flavor_name"    {}
-variable "key_pair"       {}
+variable "suffix" {
+  description = "Suffix for resource names (use net ID)"
+  type        = string
+  nullable = false
+}
+
+variable "key" {
+  description = "Name of key pair"
+  type        = string
+  default     = "id_rsa_chameleon"
+}
+
+variable "nodes" {
+  type = map(string)
+  default = {
+    "node1" = "192.168.1.11"
+    "node2" = "192.168.1.12"
+    "node3" = "192.168.1.13"
+  }
+}
