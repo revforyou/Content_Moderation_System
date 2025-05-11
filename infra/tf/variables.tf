c8b448032ca0c00a1104c6118607397cd73cbfdc
krish9164 <112063273+krish9164@users.noreply.github.com>
2025-05-11 08:24:00 -0400

    Create variables.tf

---
+++ infra/tf/variables.tf
@@ -0,0 +1,8 @@
+variable "auth_url"       {}
+variable "os_username"    {}
+variable "os_password"    {}
+variable "project_name"   {}
+variable "region"         {}
+variable "image_name"     {}
+variable "flavor_name"    {}
+variable "key_pair"       {}
