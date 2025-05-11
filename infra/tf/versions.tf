e3ecf1c77d8186b12f25b4f12ebcd63f7a61d09a
krish9164 <112063273+krish9164@users.noreply.github.com>
2025-05-11 08:25:37 -0400

    Create versions.tf

---
+++ infra/tf/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.49.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
