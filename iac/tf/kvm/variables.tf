8f08f69b349dcde3ad956af2f9de70f9639ae971
krish9164 <112063273+krish9164@users.noreply.github.com>
2025-05-11 09:58:39 -0400

    Update variables.tf

--- iac/tf/kvm/variables.tf
+++ iac/tf/kvm/variables.tf
@@ -1,20 +1,21 @@
 variable "suffix" {
   description = "Suffix for resource names (use net ID)"
   type        = string
-  nullable = false
+  nullable    = false
 }
 
 variable "key" {
   description = "Name of key pair"
   type        = string
-  default     = "id_rsa_chameleon"
+  default     = "id_rsa_chameleon_project26"
 }
 
 variable "nodes" {
   type = map(string)
   default = {
-    "node1" = "192.168.1.11"
-    "node2" = "192.168.1.12"
-    "node3" = "192.168.1.13"
+    "node-data"    = "192.168.1.11"
+    "node-monitor" = "192.168.1.12"
+    "node-train"   = "192.168.1.13"
+    "node-infer"   = "192.168.1.14"
   }
 }
