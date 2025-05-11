77783c1e37d6987dd288d375588fda8444304744
krish9164 <112063273+krish9164@users.noreply.github.com>
2025-05-11 09:56:19 -0400

    Update outputs.tf

--- iac/tf/kvm/outputs.tf
+++ iac/tf/kvm/outputs.tf
@@ -3,3 +3,10 @@ output "floating_ip_out" {
   value       = openstack_networking_floatingip_v2.floating_ip.address
 }
 
+output "instance_ips" {
+  description = "Fixed IPs of all nodes"
+  value = {
+    for name, inst in openstack_compute_instance_v2.nodes :
+    name => inst.network[0].fixed_ip_v4
+  }
+}
